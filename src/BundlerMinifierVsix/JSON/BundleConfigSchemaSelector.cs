00aac838151c9bbbd98e1972fd65a33f98580af3
Mads Kristensen <post@madskristensen.net>
2015-06-05 15:39:31 -0700

    New JSON support. Fixed #2 and fixed #3

---
+++ src/BundlerMinifierVsix/JSON/BundleConfigSchemaSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.JSON.Core.Schema;
+
+namespace BundlerMinifierVsix.JSON
+{
+    [Export(typeof(IJSONSchemaSelector))]
+    class BundleConfigSchemaSelector : IJSONSchemaSelector
+    {
+        public event EventHandler AvailableSchemasChanged;
+
+        public Task<IEnumerable<string>> GetAvailableSchemasAsync()
+        {
+            return Task.FromResult(Enumerable.Empty<string>());
+        }
+
+        public string GetSchemaFor(string fileLocation)
+        {
+            string fileName = Path.GetFileName(fileLocation);
+
+            if (!fileName.Equals("bundleconfig.json", StringComparison.OrdinalIgnoreCase))
+                return null;
+            
+            string assembly = Assembly.GetExecutingAssembly().Location;
+            string folder = Path.GetDirectoryName(assembly);
+            return Path.Combine(folder, "json\\bundleconfig-schema.json");
+        }
+    }
+}
---
+++ src/BundlerMinifierVsix/JSON/RelativeFilePathFormatProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.JSON.Core.Parser.TreeItems;
+using Microsoft.JSON.Core.Schema;
+
+namespace BundlerMinifierVsix.JSON
+{
+    [Export(typeof(IJSONSchemaFormatHandler))]
+    class RelativeFilePathFormatProvider : JSONSchemaFormatHandlerBase
+    {
+        public override string FormatName
+        {
+            get { return "relativepath"; }
+        }
+
+        public override bool HideFormatNameCompletion
+        {
+            get { return true; }
+        }
+        
+        public override IEnumerable<string> GetIssues(JSONDocument doc, string canonicalizedValue)
+        {
+            if (string.IsNullOrEmpty(doc.DocumentLocation))
+                yield break;
+
+            string fileName = Path.GetFileName(doc.DocumentLocation);
+
+            if (!fileName.Equals("bundleconfig.json", StringComparison.OrdinalIgnoreCase))
+                yield break;
+
+            string folder = Path.GetDirectoryName(doc.DocumentLocation);
+            string absolutePath = Path.Combine(folder, canonicalizedValue);
+
+            if (!File.Exists(absolutePath) && !Directory.Exists(absolutePath))
+                yield return $"The file or directory '{canonicalizedValue}' does not exist";
+
+        }
+    }
+}
