ba5086daeae66a52dc6d53de626c5224e57278b8
Andrey Taritsyn <taritsyn@gmail.com>
2015-06-11 15:21:06 +0300

    Add `CustomAngularDirectiveList` and `ProcessableScriptTypeList` properties to HTML options

--- src/BundlerMinifier/Minify/HtmlOptions.cs
+++ src/BundlerMinifier/Minify/HtmlOptions.cs
@@ -1,4 +1,5 @@
-﻿using WebMarkupMin.Core;
+﻿using Newtonsoft.Json.Linq;
+using WebMarkupMin.Core;
 using WebMarkupMin.Core.Settings;
 
 namespace BundlerMinifier
@@ -12,12 +13,14 @@ namespace BundlerMinifier
             settings.RemoveRedundantAttributes = GetValue(bundle, "removeRedundantAttributes") == "True";
 
             settings.CollapseBooleanAttributes = GetValue(bundle, "collapseBooleanAttributes") == "True";
+            settings.CustomAngularDirectiveList = GetValue(bundle, "customAngularDirectiveList");
             settings.MinifyAngularBindingExpressions = GetValue(bundle, "minifyAngularBindingExpressions") == "True";
             settings.MinifyEmbeddedCssCode = GetValue(bundle, "minifyEmbeddedCssCode") == "True";
             settings.MinifyEmbeddedJsCode= GetValue(bundle, "minifyEmbeddedJsCode") == "True";
             settings.MinifyInlineCssCode = GetValue(bundle, "minifyInlineCssCode") == "True";
             settings.MinifyInlineJsCode = GetValue(bundle, "minifyInlineJsCode") == "True";
             settings.MinifyKnockoutBindingExpressions = GetValue(bundle, "minifyKnockoutBindingExpressions") == "True";
+            settings.ProcessableScriptTypeList = GetValue(bundle, "processableScriptTypeList");
             settings.RemoveHtmlComments = GetValue(bundle, "removeHtmlComments") == "True";
             settings.RemoveTagsWithoutContent = GetValue(bundle, "removeTagsWithoutContent") == "True";
 
@@ -36,7 +39,17 @@ namespace BundlerMinifier
         internal static string GetValue(Bundle bundle, string key)
         {
             if (bundle.Minify.ContainsKey(key))
-                return bundle.Minify[key].ToString();
+            {
+                object value = bundle.Minify[key];
+                if (value is JArray)
+                {
+                    return string.Join(",", ((JArray)value).Values<string>());
+                }
+                else
+                {
+                    return value.ToString();
+                }
+            }
 
             return string.Empty;
         }
