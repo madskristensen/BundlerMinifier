9acd053778292810e61bc123afeb2fbc9a678441
Andrey Taritsyn <taritsyn@gmail.com>
2017-04-01 20:47:24 +0300

    In CSS and JavaScript options added `ignorableErrors` property

--- src/BundlerMinifier.Core/Minify/CssOptions.cs
+++ src/BundlerMinifier.Core/Minify/CssOptions.cs
@@ -8,6 +8,7 @@ namespace BundlerMinifier
         public static CssSettings GetSettings(Bundle bundle)
         {
             CssSettings settings = new CssSettings();
+            settings.IgnoreErrorList = GetValue(bundle, "ignorableErrors", string.Empty);
             settings.TermSemicolons = GetValue(bundle, "termSemicolons") == "True";
 
             string cssComment = GetValue(bundle, "commentMode");
