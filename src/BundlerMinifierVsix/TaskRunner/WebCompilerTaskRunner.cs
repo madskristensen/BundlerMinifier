67bc7ce156f56b98cd3f8b26283b461569a2869c
rockstardev <rockstardev@users.noreply.github.com>
2018-05-01 15:37:31 -0500

    Fixing race condition now that Package is async
    Ref: https://github.com/madskristensen/BundlerMinifier/issues/351
    
    Co-authored-by: Stanislav Atanasov <mastxp@gmail.com>

--- src/BundlerMinifierVsix/TaskRunner/WebCompilerTaskRunner.cs
+++ src/BundlerMinifierVsix/TaskRunner/WebCompilerTaskRunner.cs
@@ -34,6 +34,7 @@ namespace BundlerMinifierVsix
 
         public async Task<ITaskRunnerConfig> ParseConfig(ITaskRunnerCommandContext context, string configPath)
         {
+            await BundlerMinifierPackage.Package.Task;
             return await Task.Run(() =>
             {
                 if (!BundlerMinifierPackage.Options.EnableTaskRunnerExplorer)
