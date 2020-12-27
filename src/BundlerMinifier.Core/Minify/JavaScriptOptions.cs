3abdfe7d789df3f9a63ba3403187f6a632bdfccd
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2020-12-28 01:24:29 +0200

    Support ignoreErrorList in bundle definitions and pass it to NUglify.

--- src/BundlerMinifier.Core/Minify/JavaScriptOptions.cs
+++ src/BundlerMinifier.Core/Minify/JavaScriptOptions.cs
@@ -39,6 +39,8 @@ namespace BundlerMinifier
             if (int.TryParse(indentSize, out size))
                 settings.IndentSize = size;
 
+            settings.IgnoreErrorList = GetValue(bundle, "ignoreErrorList", "");
+
             return settings;
         }
 
